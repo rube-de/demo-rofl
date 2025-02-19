aa030387998d863bb48b62e0034b868b866f596b
Jernej Kos <jernej@kos.mx>
2025-02-19 15:26:12 +0100

    docker: Sleep in case fetch fails

--- docker/app.sh
+++ docker/app.sh
@@ -4,6 +4,7 @@ while true; do
 	# Fetch a recent price from Binance.
 	price=$(curl -s "https://www.binance.com/api/v3/ticker/price?symbol=${TICKER}" | jq '(.price | tonumber) * 1000000')
 	if [ -z "$price" ]; then
+		sleep 15
 		continue
 	fi
 
